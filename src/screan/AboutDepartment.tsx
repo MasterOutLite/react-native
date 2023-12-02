864adbbf17daa2d41efea9a24de0339dfe1905df
Legenda <schurik458@gmail.com>
2023-12-02 18:43:11 +0200

    ScrollView

--- src/screan/AboutDepartment.tsx
+++ src/screan/AboutDepartment.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 
 function AboutDepartment() {
   return (
-    <View style={{
-      paddingVertical: 20
+    <ScrollView style={{
+      paddingHorizontal: 16,
+      marginVertical: 30
     }}>
-      <Text variant="headlineMedium">Про кафедру</Text>
-      <Text variant="headlineSmall">Кафедра «Програмне забезпечення автоматизованих систем»</Text>
-      <Text variant="labelLarge">
+      <Text variant="headlineLarge">Про кафедру</Text>
+      <Text style={{ marginBottom: 16 }} variant="headlineMedium">Кафедра «Програмне забезпечення автоматизованих
+        систем»</Text>
+      <Text variant="headlineSmall">
         Кафедра «Програмного забезпечення автоматизованих систем» створена у 1998 році в результаті реорганізації
         кафедри «Обчислювальної техніки» та організації факультету ФІТІС, якому вона належить. Кафедра є випусковою зі
         спеціальності 121– «Інженерія програмногозабезпечення».</Text>
-      <Text variant="labelLarge">Інженерія програмного забезпечення – це дисципліна, яка пов’язана зі всіма аспектами
+      <Text variant="headlineSmall">Інженерія програмного забезпечення – це дисципліна, яка пов’язана зі всіма аспектами
         виробництва програмного забезпечення від початкових стадій створення специфікації до підтримки системи після
         здачі в експлуатацію.</Text>
 
-      <Text variant="labelLarge"> Освіта в сфері інженерії програмного забезпечення – це розумна інвестиція у своє
+      <Text variant="headlineSmall"> Освіта в сфері інженерії програмного забезпечення – це розумна інвестиція у своє
         майбутнє. У нас Ви навчитеся не просто програмувати (кодувати), а створювати програмні продукти – аналізувати
         вимоги, працювати в командні розробників, тестувати та забезпечувати якість продукту, керувати програмними
         проектами.</Text>
-      <Text variant="labelLarge">Кінець. Розходимось дітки!</Text>
-    </View>
+      <Text variant="headlineSmall">Кінець. Розходимось дітки!</Text>
+    </ScrollView>
   );
 }
 
--- src/screan/Links/Links.tsx
+++ src/screan/Links/Links.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Button, Linking, View } from "react-native";
+import { Button, Linking, ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 
 function Links() {
   return (
-    <View>
+    <ScrollView style={{ paddingHorizontal: 8 }}>
       <Text variant="displaySmall">Силки на кафедри</Text>
       <View style={{ display: "flex", gap: 20, paddingVertical: 20 }}>
         <Button title={"Посилання на університет"} onPress={() => Linking.openURL("https://chdtu.edu.ua/")} />
         <Button title={"Посилання на кафедру"} onPress={() => Linking.openURL("https://pzas.chdtu.edu.ua/")} />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
